290908d4a73c7452c6678bf20f039c67f0736bc4
abarna10303 <abarnas361@gmail.com>
2022-12-13 17:02:22 +0530

    Multiplication

---
+++ main.js
@@ -0,0 +1,7 @@
+/*Multiplication PROGRAM*/
+var num1,num2,multi;
+num1=parseInt(prompt("Enter the First Number"));
+num2=parseInt(prompt("Enter the second Number"));
+multi=num1*num2;
+console.log("Multiplication of Two number is : " +multi);
+
